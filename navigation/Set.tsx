18d66e60504af84e01ec9c84dcff2414533b15f5
김지훈 <kigpand@gmail.com>
2023-10-15 16:14:04 +0900

    start set animation 적용 성공

--- navigation/Set.tsx
+++ navigation/Set.tsx
@@ -1,18 +1,41 @@
-import {Pressable, StyleSheet, Text, View} from 'react-native';
+import {Animated, Pressable, StyleSheet, Text, View} from 'react-native';
 import {mainColor} from '../style/color';
-import {useState} from 'react';
+import {useEffect, useRef, useState} from 'react';
 import SetList from '../components/SetList';
 
 export default function Set() {
   const [exercise, setExercise] = useState<boolean>(false);
+  const animation = useRef(new Animated.Value(1)).current;
+
+  const scaleLarge = Animated.timing(animation, {
+    toValue: 1.2,
+    useNativeDriver: true,
+  });
+
+  const scaleSmall = Animated.timing(animation, {
+    toValue: 1,
+    useNativeDriver: true,
+  });
+
+  useEffect(() => {
+    Animated.loop(Animated.sequence([scaleLarge, scaleSmall])).start();
+  }, [animation]);
+
   return (
     <View style={styles.guide}>
       {exercise ? (
         <SetList />
       ) : (
-        <Pressable onPress={() => setExercise(true)}>
-          <Text style={styles.text}>시작!!!</Text>
-        </Pressable>
+        <View style={styles.container}>
+          <Text style={styles.title}>
+            운동을 시작하실려면 아래 버튼을 클릭해주세요
+          </Text>
+          <Pressable onPress={() => setExercise(true)}>
+            <Animated.View style={{transform: [{scale: animation}]}}>
+              <Text style={styles.text}>시작!!!</Text>
+            </Animated.View>
+          </Pressable>
+        </View>
       )}
     </View>
   );
@@ -26,8 +49,17 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     backgroundColor: mainColor,
   },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
   text: {
-    fontSize: 30,
+    fontSize: 20,
     fontWeight: '700',
     color: 'white',
   },
