5edff0476299220b2b231d2df53e0a58d03ea498
김지훈 <kigpand@gmail.com>
2023-10-23 21:54:56 +0900

    운동 등록 후 바로 실행할지 홈으로 돌아갈지 처리

---
+++ components/NewDataBtns.tsx
@@ -0,0 +1,71 @@
+import {Pressable, StyleSheet, Text, View} from 'react-native';
+import {falseColor, trueColor} from '../style/color';
+
+type Props = {
+  onPlaySubmit: () => void;
+  onNoSubmit: () => void;
+};
+
+export default function NewDataBtns({onPlaySubmit, onNoSubmit}: Props) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.body}>
+        <Text style={styles.title}>운동을 바로 시작하시겠습니까?</Text>
+        <View style={styles.buttons}>
+          <Pressable style={styles.button} onPress={onPlaySubmit}>
+            <Text style={styles.yesBtn}>네</Text>
+          </Pressable>
+          <Pressable style={styles.button} onPress={onNoSubmit}>
+            <Text style={styles.noBtn}>아니오</Text>
+          </Pressable>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    zIndex: 1,
+    backgroundColor: 'rgba(0,0,0,0.4)',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  body: {
+    paddingHorizontal: 20,
+    paddingVertical: 30,
+    backgroundColor: 'white',
+    borderRadius: 12,
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  buttons: {
+    marginTop: 30,
+    flexDirection: 'row',
+    gap: 10,
+  },
+  button: {
+    overflow: 'hidden',
+    borderRadius: 8,
+  },
+  yesBtn: {
+    width: 80,
+    paddingVertical: 10,
+    backgroundColor: trueColor,
+    color: 'white',
+    textAlign: 'center',
+  },
+  noBtn: {
+    width: 80,
+    paddingVertical: 10,
+    backgroundColor: falseColor,
+    color: 'white',
+    textAlign: 'center',
+  },
+});
--- navigation/NewDataMain.tsx
+++ navigation/NewDataMain.tsx
@@ -12,18 +12,28 @@ import NewDataList from '../components/NewDataList';
 import NewDataAdd from '../components/NewDataAdd';
 import useExercise from '../store/useExercise';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack/lib/typescript/src/types';
+import StartModal from '../components/modal/StartModal';
+import NewDataBtns from '../components/NewDataBtns';
 
 export default function NewDataMain() {
   const route = useRoute<RouteProp<any>>();
   const [routineArr, setRoutineArr] = useState<IRoutineData[]>([]);
   const {addList} = useExercise();
   const nav = useNavigation<NativeStackNavigationProp<ParamListBase>>();
+  const [onBtn, setOnBtn] = useState<boolean>(false);
+  const [start, setStart] = useState<boolean>(false);
 
   function getRoutineArr(routine: IRoutineData) {
     setRoutineArr([...routineArr, routine]);
   }
 
-  function onSubmit() {
+  function onPlaySubmit() {
+    setOnBtn(false);
+    setStart(true);
+  }
+
+  function onNoSubmit() {
+    setOnBtn(false);
     if (!route.params) return;
     addList({title: route.params.title, routine: routineArr});
     nav.navigate('Home');
@@ -35,10 +45,18 @@ export default function NewDataMain() {
       <NewDataAdd getRoutineArr={getRoutineArr} />
       <NewDataList routineArr={routineArr} />
       {routineArr.length > 0 && (
-        <Pressable style={styles.button} onPress={onSubmit}>
+        <Pressable style={styles.button} onPress={() => setOnBtn(true)}>
           <Text style={{color: 'white'}}>등록</Text>
         </Pressable>
       )}
+      {onBtn && (
+        <NewDataBtns onPlaySubmit={onPlaySubmit} onNoSubmit={onNoSubmit} />
+      )}
+      <StartModal
+        routine={{title: route.params!.title, routine: routineArr}}
+        onView={start}
+        onCloseView={() => setStart(false)}
+      />
     </View>
   );
 }
